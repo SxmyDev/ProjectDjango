7e87813f97cae2e70b75c234bba1f4b15c0d2097
SxmyDev <144488150+SxmyDev@users.noreply.github.com>
2023-09-10 18:42:25 +0200

    Plantillas

--- klassifieds/main/urls.py
+++ klassifieds/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('welcome/', views.welcome, name='welcome')
+    path('welcome/', views.welcome, name='welcome'),
+    path('home/', views.home, name='home'),
 ]
